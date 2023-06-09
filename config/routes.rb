a1bdca012bbc12ac0932d514af5ff86765ecaf4d
dfsalam <dfsalam@gmail.com>
2023-06-09 01:54:58 -0500

    Refactor routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :greetings
+      resources :greetings, only: [:index]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
